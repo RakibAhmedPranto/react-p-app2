10a2c0be9e8fd0fb38a4a6a8f9ad78aa5660b551
Rakib Ahmed <rakib@ghit.com>
2022-11-07 13:55:36 +0600

    home page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-
+import Footer from './components/Footer';
+import Header from './components/Header';
+import Home from './components/Home';
+import Videos from './components/Videos';
 
 function App() {
   return (
     <Router>
+      <Header />
       <Routes>
-        <Route />
+        <Route path="/" element={<Home />} />
+        <Route path="/videos" element={<Videos />} />
       </Routes>
+      <Footer />
     </Router>
   );
 }
